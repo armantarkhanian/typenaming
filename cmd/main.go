a407bc5a5735f59c707b90ef38fef1dabed01e04
Arman Tarkhanian <armantarkhanian@gmail.com>
2022-04-30 22:59:04 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/typenaming/typenaming/pkg/analyzer"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+func main() {
+	singlechecker.Main(analyzer.Analyzer)
+}
---
+++ pkg/analyzer.go
@@ -0,0 +1,56 @@
+package analyzer
+
+import (
+	"go/ast"
+	"strings"
+
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "typenaming",
+	Doc:      "Checks that user declared types have not the `Type` suffix.",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.TypeSpec)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
+		typeDecl, ok := node.(*ast.TypeSpec)
+		if !ok {
+			return
+		}
+
+		if !strings.HasSuffix(typeDecl.Name.Name, "Type") {
+			return
+		}
+
+		pass.Report(analysis.Diagnostic{
+			Pos:     typeDecl.Name.End() - 4,
+			Message: "do not use suffix `Type` in Go",
+			SuggestedFixes: []analysis.SuggestedFix{
+				{
+					Message: "trim suffix `Type`",
+					TextEdits: []analysis.TextEdit{
+						{
+							Pos:     typeDecl.Name.End() - 4,
+							End:     typeDecl.Name.End(),
+							NewText: []byte{},
+						},
+					},
+				},
+			},
+		})
+	})
+
+	return nil, nil
+}
